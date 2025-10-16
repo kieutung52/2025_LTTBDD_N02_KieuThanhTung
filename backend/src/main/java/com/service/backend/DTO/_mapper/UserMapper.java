9474a06c69687993e21a26dfe80562ff61c0f49b
kieutung52 <kieuthanhtung0502@gmail.com>
2025-10-16 23:49:08 +0700

    using mapper data

---
+++ backend/src/main/java/com/service/backend/DTO/_mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.service.backend.DTO._mapper;
+
+import org.mapstruct.Mapper;
+
+import com.service.backend.DTO.DataTransform.response.auth.RegisterResponse;
+import com.service.backend.DTO.DataTransform.response.user.UserResponse;
+import com.service.backend.models.User;
+
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserResponse toUserResponse(User user);
+
+    @Mapping(target = "registered", ignore = true)
+    RegisterResponse toRegisterResponse(User user);
+}
